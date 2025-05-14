634f43a3dcc16f95ad75d8ca412da850f78cfe69
filatof <filatof@gmail.com>
2025-05-14 20:57:36 +0300

    modify output info

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,19 +1,6 @@
-output "vm_info" {
-  description = "Names and IP addresses of the created VMs"
+output "lxc_info" {
   value = {
-    for i in range(var.vm_count) :
-    format("vm-%02d", i + 1) => proxmox_virtual_environment_vm.vm[i].ipv4_addresses[1]
+    for i in range(var.kube_count) :
+    format("k8s-node-%02d", i + 1) => proxmox_lxc.kubernetes[i].network[0].ip
   }
-}
-
-#создаем файл инвентаря для ansible
-resource "local_file" "ansible_inventory" {
-  filename = "hosts"
-  content = join("\n", [
-    for i in range(var.vm_count) : format(
-      "server%d ansible_host=192.168.1.%d ansible_user=fill ansible_port=22 ansible_private_key_file=/Users/fill/.ssh/id_ed25519",
-      i + 101 ,
-      i + 101
-    )
-  ])
 }
